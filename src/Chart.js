411b953a722a2aed60dac4a71548cc328a151b0c
pavlelekic <pavle.lekic@toptal.com>
2021-06-14 01:37:00 +0200

    Fixed overflow and padding issues

--- src/Chart.js
+++ src/Chart.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Chart } from "react-charts";
 import AspectRatio from "./AspectRatio";
+import './Chart.css';
 
 const series = {
   type: "area"
 };
 
 const axes = [
-  { primary: true, position: "bottom", type: "linear" },
+  { primary: true, position: "bottom", type: "linear", format: value => `${value} (${2021 + parseInt(value, 10)})` },
   { position: "left", type: "linear", stacked: true }
 ];
 
@@ -27,17 +28,12 @@ const AreaChart = React.memo(({ calculatedInvestments }) => {
         secondary: monthlyInvestmentsSum
       }))
     },
-    {
-      label: 'Total', 
-      data: calculatedInvestments.map(({ initialSum, monthlyInvestmentsSum }, index) => ({
-        primary: index + 1,
-        secondary: initialSum + monthlyInvestmentsSum
-      }))
-    },
   ];
   return (
     <AspectRatio ratio={0.45}>
-      <Chart data={data} series={series} axes={axes} tooltip />
+      <div style={{width: 'calc(100% - 21px)', height: '100%', margin: '0 21px 0 0px'}}>
+        <Chart data={data} series={series} axes={axes} tooltip/>
+      </div>
     </AspectRatio>
   );
 });
