bacf64aecb932eb83d0c0d423016e5233910618f
pavlelekic <pavle.lekic@toptal.com>
2021-06-13 18:02:11 +0200

    Added monthly investment calc

--- src/App.js
+++ src/App.js
@@ -10,16 +10,18 @@ const ResultsTable = ({ calculatedInvestments }) => {
         <Table.Row>
           <Table.HeaderCell>Year</Table.HeaderCell>
           <Table.HeaderCell>Initial sum</Table.HeaderCell>
+          <Table.HeaderCell>Monthly investments sum</Table.HeaderCell>
           <Table.HeaderCell>Total amount</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
   
       <Table.Body>
-        {calculatedInvestments.map(({ initialSum }, index) => (
+        {calculatedInvestments.map(({ initialSum, monthlyInvestmentsSum }, index) => (
           <Table.Row>
             <Table.Cell>{`${index + 1} (${currentYear + index + 1})`}</Table.Cell>
             <Table.Cell>{(initialSum).toFixed(1)}</Table.Cell>
-            <Table.Cell>{(initialSum).toFixed(1)}</Table.Cell>
+            <Table.Cell>{(monthlyInvestmentsSum).toFixed(1)}</Table.Cell>
+            <Table.Cell>{(initialSum + monthlyInvestmentsSum).toFixed(1)}</Table.Cell>
           </Table.Row>
         ))}
       </Table.Body>
@@ -33,11 +35,15 @@ const calculateInvestments = ({initialSum, investmentPerMonth, growthRatePerMont
 
   for (let year = 1; year <= 40; year++) {
     const previousInitialSum = year > 1 ? resultArr[year - 2].initialSum : initialSum;
+    const previousMonthlyInvestmentsSum = year > 1 ? resultArr[year - 2].monthlyInvestmentsSum : 0;
     const currentYearResults = resultArr[year - 1] = {
-      initialSum: previousInitialSum
+      initialSum: previousInitialSum,
+      monthlyInvestmentsSum: previousMonthlyInvestmentsSum,
     };
     for (let month = 1; month <= 12; month++) {
       currentYearResults.initialSum *= growthFactorPerMonth;
+      currentYearResults.monthlyInvestmentsSum *= growthFactorPerMonth;
+      currentYearResults.monthlyInvestmentsSum += investmentPerMonth;
     }
   }
 
@@ -54,11 +60,11 @@ function App() {
     <Container>
       <Header as='h1'>The Investment Calc</Header>
       <Form onSubmit={() => {
-        const investmentPerMonth = investmentPerMonthInput.current.value;
-        const growthRatePerMonth = growthRatePerMonthInput.current.value;
-        const initialSum = initialSumInput.current.value;
+        const investmentPerMonth = parseFloat(investmentPerMonthInput.current.value);
+        const growthRatePerMonth = parseFloat(growthRatePerMonthInput.current.value);
+        const initialSum = parseFloat(initialSumInput.current.value);
 
-        if (investmentPerMonth > 0 && growthRatePerMonth > 0) {
+        if (investmentPerMonth >= 0 && growthRatePerMonth > 0) {
           const result = calculateInvestments({initialSum, investmentPerMonth, growthRatePerMonth});
           setCalculatedInvestments(result);
         }
@@ -68,11 +74,11 @@ function App() {
       }}>
         <Form.Field>
           <label>Initial (starting) sum</label>
-          <input placeholder='50,000' ref={initialSumInput} defaultValue={0} />
+          <input placeholder='50,000' ref={initialSumInput} type="number" min="0" />
         </Form.Field>
         <Form.Field>
           <label>Amount of investment per month</label>
-          <input placeholder='1000' ref={investmentPerMonthInput}/>
+          <input placeholder='1000' ref={investmentPerMonthInput} type="number" min="0" />
         </Form.Field>
         <Form.Field>
           <label>Growth rate in % per month</label>
@@ -81,6 +87,7 @@ function App() {
             ref={growthRatePerMonthInput}
             label={{ basic: true, content: '%' }}
             labelPosition='right'
+            type="number" step="0.01" min="0" max="100"
           />
         </Form.Field>
         <Button type='submit'>Calculate</Button>
